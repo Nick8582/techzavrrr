4e4f562ecb7444970181a05f4b439b9439b848cc
Potapov Vladislav <89172111013q@gmail.com>
2022-10-02 00:34:51 +0300

    Создание формы заказа,настройка миксинов для полей ввода

--- magazine/src/components/BaseFormText.vue
+++ magazine/src/components/BaseFormText.vue
@@ -4,7 +4,6 @@
   <BaseFormField :title="title" :error="error">
     <input
       class="form__input"
-      type="text"
       :type="type"
       v-model="dataValue"
       :placeholder="placeholder">
