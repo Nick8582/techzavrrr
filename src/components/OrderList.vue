ba17f1c4da1f2f2287f212f194bcac746e31e23f
Potapov Vladislav <89172111013q@gmail.com>
2022-10-05 21:59:27 +0300

    Исправление ошибки

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -6,17 +6,24 @@
     <ul class="cart__orders">
       <li class="cart__order" v-for="item in products" :key="item.productId">
         <h3>{{ item.product.title }}</h3>
-        <b>{{ (item.product.price * item.quantity) | numberFormat }} ₽</b>
+        <b>{{ (item.product.price * (item.quantity || item.amount)) | numberFormat }} ₽</b>
         <span>Артикул: {{ item.product.id }}</span>
       </li>
     </ul>
 
     <div class="cart__total">
       <p>Доставка: <b>500 ₽</b></p>
-      <p>Итого: <b>{{ amount }}</b> товара на сумму <b>{{ totalPrice | numberFormat }} ₽</b></p>
+      <p>
+        Итого: <b>{{ amount }}</b> товара на сумму
+        <b>{{ totalPrice | numberFormat }} ₽</b>
+      </p>
     </div>
 
-    <button class="cart__button button button--primery" type="submit" v-if="button">
+    <button
+      class="cart__button button button--primery"
+      type="submit"
+      v-if="button"
+    >
       Оформить заказ
     </button>
   </div>
